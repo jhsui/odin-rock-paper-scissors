d7c9c8de0d5b9bb039b52d41a50f63067cd7141e
jhsui <jiahansui1998@gmail.com>
2025-09-22 14:50:08 -0400

    test the computer choice

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ function getComputerChoice() {
     }
 }
 
+let s = 0;
+let p = 0;
+let r = 0;
+
+let i = 1000000000;
+const total = i;
+while (i > 0) {
+    const computerChoice = getComputerChoice();
+    if (computerChoice === "scissors") {
+        s++;
+    } else if (computerChoice === "paper") {
+        p++;
+    } else if (computerChoice === "rock") {
+        r++;
+    }
+    i--;
+}
+console.log(s / total);
+console.log(p / total);
+console.log(r / total);
+
+
 //console.log(getComputerChoice());
 
 
